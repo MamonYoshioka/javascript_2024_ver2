031d7b0af29fc2c4e8fa2842e826ba7419475965
Mamon Yoshioka <yoshiokamamon@gmail.com>
2024-04-14 18:04:01 +0900

    add "文（ブロック文を含む）・式"

--- js/structure.js
+++ js/structure.js
@@ -48,3 +48,41 @@ console.log(`この論理積は'${logical_operators}'です！`);
 logical_operators = true || false;
 console.log(`この論理和は'${logical_operators}'です！`);
 
+userInput = '';
+userName = userInput || 'Please Your Name';
+console.log(userName);
+
+// null合体演算子
+/*
+null合体演算子は並列して論理演算子と使うとエラーになる。
+*/
+let usr_name = '' ?? 'Please Your Name!!';
+console.log(usr_name);
+
+// 文と式、ブロック文
+/*
+式：値を生成し、変数に代入できるものです
+例）1 + 1
+
+文：ある特定の処理を行うための1工程。
+つまり、文の末尾にはセミコロン;を使用し、それによって文を区切ることができるもの。
+例）const isTrue = true; 
+*/
+
+let text = 'abcde';
+if(text === 'abcde'){
+    console.log(`Hello ${text}`);
+}
+
+const hello = 'Hello2';
+{
+    // console.log(hello);
+    /*
+    ブロック文の場合、変数宣言前に変数を呼び出すと、
+    下記のようなエラーになる。
+
+    "ReferenceError: Cannot access 'hello' before initialization"
+     */
+    const hello = 'Hello1';
+    console.log(hello);
+}
