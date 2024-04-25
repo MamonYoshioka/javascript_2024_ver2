94155aa191e4211d7b202ca98daa7e9a794b1c8d
Mamon Yoshioka <yoshiokamamon@gmail.com>
2024-04-25 20:39:51 +0900

    add structure_6 file

--- js/structure_6.js
+++ js/structure_6.js
@@ -1,5 +1,7 @@
 /*
-
+オブジェクトの繰り返し処理を行う際に使用する
+また、配列もオブジェクト一種のため、for-inを使用できるが、
+基本的には配列などのオブジェクトを繰り返し処理したい場合はfor-of文を使うのが一般的
 */
 
 const tea = {
@@ -10,4 +12,22 @@ const tea = {
 
 for (const key in tea) {
     document.write(`${key}:&nbsp${tea[key]}<br>`);
+    if (key === 'price') {
+        document.write('break!');
+        break;
+    }
+}
+
+const coffee = {
+    name: 'Black coffee',
+    price: 300,
+    is_hot: true,
+}
+
+for (const key in coffee) {
+    if(key === 'price'){
+        document.write('continue!!');
+        continue;
+    }
+    document.write(`<br>${key}:&nbsp${tea[key]}<br>`);
 }
